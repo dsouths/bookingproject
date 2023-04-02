465e5d5829f274b60352b8453fad426e895ecc18
dsouths <dsouths@hotmail.com>
2023-04-02 13:37:33 +0000

    added base.html forms.py, urls.py, admin.py

--- BK/admin.py
+++ BK/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import Service, Booking
 
 # Register your models here.
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ("service_name", "price")
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    """
+    Class registered to represent model in admin database.
+    """
+    list_display = ('user', 'name',
+                    'date',
+                    'time',
+                    'phone',
+                    'email',
+                    'service'
+                    )
+    search_fields = ('user',
+                     'name',
+                     'date',
+                     'phone')
+    list_filter = ('user',
+                   'name',
+                   'date',
+                   'phone')
---
+++ BK/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('services/', views.services, name="services"),
+    path('booknow/', views.booknow, name="booknow"),
+    path('bookings/', views.bookings, name='bookings'),
+    path('change/<int:booking_id>/', views.change_booking,
+         name='change_booking'),
+    path('delete-booking/<int:booking_id>/', views.delete_booking,
+         name='delete_booking'),
+]
