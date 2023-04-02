89e74ea8fb7f869cdd914315af6ed59918599d48
dsouths <dsouths@hotmail.com>
2023-04-02 13:28:56 +0000

    added & populated forms.py

--- BK/views.py
+++ BK/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import *
+from .forms import BookingForm
+import datetime
+from django.contrib import messages
+
 
 # Create your views here.
--- BOOKING/settings.py
+++ BOOKING/settings.py
@@ -32,19 +32,17 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["bookingproject.herokuapp.com", "localhost"]
 
-
-
 # Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
-    'cloudinary_storage',
-    'django.contrib.staticfiles',
-    'cloudinary',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
+    'django.contrib.staticfiles',
+    'cloudinary',
     'BK',
 ]
 
@@ -133,8 +131,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
